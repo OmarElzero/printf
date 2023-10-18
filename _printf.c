8cb778a66964d9a2f722cc404c5443c40d7c7ea9
OmarElzero <omarmohmed7659944@gmail.com>
2023-10-17 17:55:28 -0700

    150

--- _printf.c
+++ _printf.c
@@ -1,44 +1,45 @@
 #include <stdarg.h>
 #include <stdio.h>
-
 int _printf(const char *format, ...) {
-   int count = 0;
-   va_list args;
-   va_start(args, format);
-
-   for (const char *p = format; *p; ++p) {
-       if (*p != '%') {
-           putchar(*p);
-           count++;
-       } else {
-           p++;
-           switch (*p) {
-               case 'c': {
-                   char c = (char) va_arg(args, int);
-                   putchar(c);
-                   count++;
-                   break;
-               }
-               case 's': {
-                   const char *s = va_arg(args, const char *);
-                   for (const char *sp = s; *sp; ++sp) {
-                       putchar(*sp);
-                       count++;
-                   }
-                   break;
-               }
-               case '%': {
-                   putchar('%');
-                   count++;
-                   break;
-               }
-               default: {
-                   break;
-               }
-           }
-       }
-   }
-
-   va_end(args);
-   return count;
+    int count = 0;
+    va_list args;
+    va_start(args, format);
+        for (const char *p = format; *p; ++p) 
+        {
+        if (*p != '%') {
+                putchar(*p);
+            count++;
+    }   else        {
+                ++p;
+                    switch (*p) {
+                    case 'c': {
+                    char c = (char) va_arg(args, int);
+                    putchar(c);
+                        count++;
+                    break;
 }
+                    case 's': {
+                    const char *s = va_arg(args, const char *);
+                    for (const char *sp = s; *sp; ++sp) {
+                    putchar(*sp);
+                        count++;
+                        }
+                        break;
+}
+                    case '%': {
+                        putchar('%');
+                            count++;
+                                break;
+                                }
+                    default: {
+                    // ignore invalid format specifiers
+                                break;
+                                }            
+                                }
+        }
+    }
+
+    va_end(args);
+        return count;
+}
+
