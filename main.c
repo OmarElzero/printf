4da1d997590964beee67eba3ca2b9ed306f36bcf
OmarElzero <omarmohmed7659944@gmail.com>
2023-10-21 15:22:46 -0700

    last hope

--- main.c
+++ main.c
@@ -10,17 +10,17 @@
  */
 int main(void)
 {
-	int lenth1;
-	int my_lenth2;
+	int len;
+	int len2;
 	unsigned int ui;
 	void *addr;
 
-	lenth1 = _printf("Let's try to printf a simple sentence.\n");
-	my_lenth2 = printf("Let's try to printf a simple sentence.\n");
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
 ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", lenth1, lenth1);
-	printf("Length:[%d, %i]\n", my_lenth2, my_lenth2);
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
 	_printf("Unsigned:[%u]\n", ui);
@@ -35,10 +35,10 @@ ui = (unsigned int)INT_MAX + 1024;
 	printf("String:[%s]\n", "I am a string !");
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
-	lenth1 = _printf("Percent:[%%]\n");
-	my_lenth2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", lenth1);
-	printf("Len:[%d]\n", my_lenth2);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 	 _printf("Unknown:[%r]\n");
 	return (0);
 }
